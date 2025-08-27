659d4b752319a43145d60540fb344f85c9b334fb
RayOjuka <rayweathers88@gmail.com>
2025-08-27 09:39:18 +0300

    objects

--- index.js
+++ index.js
@@ -1,44 +1,64 @@
-const friends = ['Michael', 'Sarah', 'Jessica'];
-// add a new friend to the array
-friends.push('John');
-console.log(friends); // ['Michael', 'Sarah', 'Jessica', 'John']
-// add elements at the biginning of the array
-friends.unshift('Alice');
-console.log(friends); // ['Alice', 'Michael', 'Sarah', 'Jessica', 'John']
+// const friends = ['Michael', 'Sarah', 'Jessica'];
+// // add a new friend to the array
+// friends.push('John');
+// console.log(friends); // ['Michael', 'Sarah', 'Jessica', 'John']
+// // add elements at the biginning of the array
+// friends.unshift('Alice');
+// console.log(friends); // ['Alice', 'Michael', 'Sarah', 'Jessica', 'John']
 
-// remove element from the end of the array
-friends.pop();
-console.log(friends); // ['Alice', 'Michael', 'Sarah', 'Jessica']
+// // remove element from the end of the array
+// friends.pop();
+// console.log(friends); // ['Alice', 'Michael', 'Sarah', 'Jessica']
 
-// remove element from the beginning of the array
-friends.shift();
-console.log(friends); // ['Michael', 'Sarah', 'Jessica']
+// // remove element from the beginning of the array
+// friends.shift();
+// console.log(friends); // ['Michael', 'Sarah', 'Jessica']
 
-console.log(friends.indexOf('Sarah')); // 1
+// console.log(friends.indexOf('Sarah')); // 1
 
-console.log(friends.includes('Jessica')); // true
+// console.log(friends.includes('Jessica')); // true
 
-if (friends.includes('John')) {
-  console.log('John is a friend');
-}
+// if (friends.includes('John')) {
+//   console.log('John is a friend');
+// }
 
-function calcTip(input) {
-    if(input >= 50 && input <= 300 ) {
-        return (0.15 * input);
-    } else {
-        return (0.2 * input);
+// function calcTip(input) {
+//     if(input >= 50 && input <= 300 ) {
+//         return (0.15 * input);
+//     } else {
+//         return (0.2 * input);
+//     }
+// }
+
+// const result = calcTip(100);
+
+// console.log(result);
+
+
+// const bills = [125, 555, 44];
+
+// const tips = bills.map((bill) => calcTip(bill));
+// console.log(tips); // [18.75, 83.25, 8.8]
+
+// const total = bills.map((bill, index) => bill + tips[index]);
+// console.log(total); // [143.75, 638.25, 52.8
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Sarah', 'Jessica'],
+    hasDriverLicense: true,
+
+    // calcAge: function (birthYear) {
+    //     return 2023 - birthYear;
+    // }
+
+    calcAge: function () {
+        return 2023 - this.birthYear;
     }
-}
+};
 
-const result = calcTip(100);
-
-console.log(result);
-
-
-const bills = [125, 555, 44];
-
-const tips = bills.map((bill) => calcTip(bill));
-console.log(tips); // [18.75, 83.25, 8.8]
-
-const total = bills.map((bill, index) => bill + tips[index]);
-console.log(total); // [143.75, 638.25, 52.8
+console.log(jonas.calcAge()); // 32
+console.log(jonas['calcAge'](1991)); // 32
